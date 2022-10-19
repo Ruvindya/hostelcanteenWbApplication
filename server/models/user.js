263cb31ae80269c2bb9e12cc06397ff16bbaee2e
Ruvindya <2019e117@eng.jfn.ac.lk>
2022-10-19 17:37:49 +0530

    firstComit

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize,DataTypes) => {
+
+
+    //Postschema
+        const LoginSchema = sequelize.define("user" , {  
+            
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+                defaultValue:"123",
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue:"EXname",
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue:"EXEmail",
+            },
+    
+            username: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue:"EXUserName",
+            },
+
+            password: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue:"******",
+            },
+        });
+    
+        return LoginSchema;
+     };
