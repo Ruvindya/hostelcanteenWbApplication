2c75080b4bee61c362ab316dc899a4a63b578c2a
Ruvindya <2019e117@eng.jfn.ac.lk>
2022-10-20 11:25:35 +0530

    second comit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/InfoUser").then((response) => {
+      //console.log(response.data);
+      setListOfUsers(response.data);
+
+    });
+  }, []);
+   
+//   return (
+//     <div className="App">
+//       <h1>Welcome</h1>
+//       {listOfUsers.map((value, key) => {
+//         return (
+//          <div className='user'> 
+//           <div className='name'> {value.name} </div>
+//           <div className='email'> {value.email} </div>
+//         </div>);
+//       })};
+//     </div>
+//   );
+// }
+
+return (
+  <div >
+              <div className='user'>
+
+                  <table>
+                          <tr>
+                              <td className='columnName'><h3>User ID</h3></td>
+                              <td className='columnName'><h3>Name</h3></td>
+                              <td className='columnName'><h3>E-mail</h3></td>
+                              <td className='columnName'> <h3>UserName</h3></td>
+
+                              
+                          </tr>
+
+                          {listOfUsers.map((value,key)=>(
+                          <tr key={key}>
+                              <td className='columnData'>{value.userId} </td> 
+                              <td className='columnData'>{value.name}</td>
+                              <td className='columnData'>{value.email}</td>
+                              <td className='columnData'>{value.userName}</td>
+
+
+                          </tr>
+
+                           ))}
+                  </table>
+                  
+              </div>
+     
+
+  </div>
+)
+{/* <td><button onClick={() => toUpdateStudent(value.Student_ID)}>Edit</button></td>
+<td><button onClick={() => handleDelete(value.Student_ID)}>Remove</button></td>  */}
+
 }
 
 export default App;
