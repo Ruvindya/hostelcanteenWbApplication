13e2de7e3aa4c66c1debc4e8f2b2941d265f0ce5
Ruvindya <2019e117@eng.jfn.ac.lk>
2022-10-21 15:39:46 +0530

    Menu: get post ,frontend backend database

--- server/routes/InfoMenu.js
+++ server/routes/InfoMenu.js
@@ -8,26 +8,44 @@ router.get("/getItem", async (req, res) => {
     res.json(listOfMenuItems);
 });
 
+router.get("/getItem/:BLD", async (req, res) => {
+  //res.send("hello world");
+  const listOfMenuItems = await menu.findAll(req.body, {
+    where: {
+      BLD: req.params.BLD
+    }
+  });
+  res.json(listOfMenuItems);
+});
+
 router.post("/postItem", async (req, res) => {
     const foodItems = req.body; 
     await menu.create(foodItems);
     res.json(foodItems);
 });
 
-router.post("/UpdateItem", async (req, res) => {
+// router.post("/UpdateItem", async (req, res) => {
     
     
-});
+// });
 
-router.put("/UpdateItem", function (req, res, next) {
-    menu.update(
-      {itemName: req.body.itemName,},
-      {where: req.params.menuID}
-    )
-    .then(function(rowsUpdated) {
-      res.json(rowsUpdated)
-    })
-    .catch(next)
+router.put("/UpdateItem/:menuID", async (req, res, next)=> {
+  const foodItems = req.body; 
+    await menu.create(foodItems, {
+      where: {
+        menuID: req.params.menuID
+      }
+    }
+      );
+
+    // menu.update(
+    //   {itemName: req.body.itemName,},
+    //   {where: req.params.menuID}
+    // )
+    // .then(function(rowsUpdated) {
+    //   res.json(rowsUpdated)
+    // })
+    // .catch(next)
    })
 
 // router.delete("/", async (req, res) => {
