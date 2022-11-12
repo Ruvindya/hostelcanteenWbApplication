46d35ff8c2f66539245a3faab842261c4f6a1f06
Ruvindya <2019e117@eng.jfn.ac.lk>
2022-11-12 23:58:28 +0530

    Home to place order

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import logo from "../logo2.png"
-
+import { useNavigate} from "react-router-dom";
 // import axios from "axios";
 // import {useEffect, useState} from "react";
 //import { useNavigate} from "react-router-dom";
 import '../App.css';
 
 function Home(){
- 
+  const navigate = useNavigate();
+  const PlaceOrder = async (e) => {
+    e.preventDefault();
+    try {
+      navigate("/CustomorRegister");    
+    
+                  
+    } catch (error) {
+   console.log(error);
+    }
+}
 
   return (
     <form >
@@ -16,6 +26,7 @@ function Home(){
         <h1 className='hungry'>HUNGRY!?</h1>
         <h4 className='Lets Dine In..'>Lets Dine In..</h4>
          <img className="logoHome" src={logo} alt="logo..."></img> 
+         <button   type="submit"  onClick={PlaceOrder} >Place Order</button>
            <div className='user' >
 
           
