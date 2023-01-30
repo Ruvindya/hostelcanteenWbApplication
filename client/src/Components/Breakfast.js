3fb02508cd8c7b23fb43b6a6685ea6d2b9dac113
Ruvindya <2019e117@eng.jfn.ac.lk>
2023-01-30 22:58:05 +0530

    until faker

--- client/src/Components/Breakfast.js
+++ client/src/Components/Breakfast.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import faker from 'faker';
+import { faker } from '@faker-js/faker';
 
 
 const Breakfast = () => {
-  const productArray = [...Array(4)].map
+  const productArray = [...Array(4)].map(()=>({
+    id:faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image()
+
+  }));
+  console.log(productArray);
   return (
     <div>Breakfast</div>
   )
